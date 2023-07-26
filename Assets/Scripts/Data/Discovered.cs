e1889b4e35e924214d9bc700bab2ab560c83843d
Felix Boucher <felix-gabriel.boucher-luneau.1@ens.etsmtl.ca>
2023-07-26 13:24:16 -0400

    update wiki

--- Assets/Scripts/Data/Discovered.cs
+++ Assets/Scripts/Data/Discovered.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using static UnityEditor.Rendering.FilterWindow;
 
 public class Discovered : MonoSingleton<Discovered> 
 {
